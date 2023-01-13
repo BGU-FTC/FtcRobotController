3eed949000261461d3ed40de87696635ff9e0ce9
JackDryer <jackjbld@gmail.com>
2023-01-13 12:18:36 +0000

    added INS

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/driving.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/driving.kt
@@ -3,12 +3,16 @@ package org.firstinspires.ftc.teamcode
 import android.content.Context
 import android.hardware.SensorManager
 import androidx.core.content.ContextCompat.getSystemService
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import com.qualcomm.robotcore.hardware.DcMotor
+import com.qualcomm.robotcore.hardware.IMU
 import com.qualcomm.robotcore.hardware.Servo
 import com.qualcomm.robotcore.util.ElapsedTime
 import org.firstinspires.ftc.robotcore.external.Telemetry
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation
 
 
 @TeleOp(name="Controller Tester", group="Tests")
@@ -28,15 +32,22 @@ class ControllerOpMode : LinearOpMode() {
             buttonNumber++
         }
         caller.actions.add(ButtonAction({addOne()},{time.seconds().toInt()%10==0}))
+        val imuparams = IMU.Parameters(RevHubOrientationOnRobot(
+            RevHubOrientationOnRobot.LogoFacingDirection.UP,
+            RevHubOrientationOnRobot.UsbFacingDirection.FORWARD
+        ))
+        val imu:IMU = hardwareMap.get("imu") as IMU;
         waitForStart()
         time.startTime()
 
         while (opModeIsActive()) {
             caller.step()
-            telemetry.addData("Time", time.seconds())
-            telemetry.addData("raw",rawNumber)
-            telemetry.addData("action",actionNumber)
-            telemetry.addData("button",buttonNumber)
+            telemetry.addData("yaw",imu.robotYawPitchRollAngles.getYaw(AngleUnit.DEGREES));
+            telemetry.addData("yawrate",imu.getRobotAngularVelocity(AngleUnit.DEGREES).yRotationRate)
+            //telemetry.addData("Time", time.seconds())
+            //telemetry.addData("raw",rawNumber)
+            //telemetry.addData("action",actionNumber)
+            //elemetry.addData("button",buttonNumber)
             telemetry.update();
         }
     }
